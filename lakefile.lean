58b17bf7cef0bc1d585e22464344ea4d0c5c84f2
Patrick Thomas <pthomas505@gmail.com>
2024-11-09 20:35:03 -0800

    Initial code.

--- lakefile.lean
+++ lakefile.lean
@@ -11,6 +11,11 @@ package «MM0_meta» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+
+require mathlib_extra from git
+  "git@github.com:pthomas505/mathlib_extra.git"
+
+
 @[default_target]
 lean_lib «MM0Meta» where
   -- add any library configuration options here
